742c0b39edd0e8139fdace489b6dc011d959170d
jason <momada@zoho.com>
2015-12-04 19:12:01 -0800

    .

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='simple',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='jason',
+    author_email='',
+    description=''
+)
